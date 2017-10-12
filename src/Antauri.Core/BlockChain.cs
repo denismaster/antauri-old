645edb79e35f3feaff9bffbca945f055a756c320
denismaster <denismaster@outlook.com>
2017-10-12 22:45:21 +0300

    Add method to check block Validity

--- src/Antauri.Core/BlockChain.cs
+++ src/Antauri.Core/BlockChain.cs
@@ -39,7 +39,27 @@ namespace Antauri.Core
 
         public bool IsValidNewBlock(Block newBlock, Block previousBlock)
         {
-            throw new NotImplementedException();
+            if (previousBlock.Index + 1 != newBlock.Index)
+            {
+                Console.WriteLine("invalid index");
+                return false;
+            }
+            else if (previousBlock.Hash != newBlock.PreviousHash)
+            {
+                Console.WriteLine("invalid previoushash");
+                return false;
+            }
+            else
+            {
+                string hash = CalculateHash(newBlock.Index, newBlock.PreviousHash, newBlock.TimeStamp,
+                        newBlock.Data);
+                if (hash != newBlock.Hash)
+                {
+                    Console.WriteLine("invalid hash: " + hash + " " + newBlock.Hash);
+                    return false;
+                }
+            }
+            return true;
         }
 
         private string CalculateHash(int index, string previousHash, long timestamp, string data)
@@ -47,6 +67,40 @@ namespace Antauri.Core
             var builder = new StringBuilder(index);
             builder.Append(previousHash).Append(timestamp).Append(data);
             return _hasher.Hash(builder.ToString());
+        }
+
+        public void ReplaceChain(List<Block> newBlocks)
+        {
+            if (IsValidBlocks(newBlocks) && newBlocks.Count > _blocks.Count)
+            {
+                _blocks = newBlocks;
+            }
+            else
+            {
+                Console.WriteLine("Received blockchain invalid");
+            }
+        }
+
+        private bool IsValidBlocks(List<Block> newBlocks)
+        {
+            Block firstBlock = newBlocks[0];
+            if (firstBlock.Equals(Block.GenesisBlock))
+            {
+                return false;
+            }
+
+            for (int i = 1; i < newBlocks.Count; i++)
+            {
+                if (IsValidNewBlock(newBlocks[i], firstBlock))
+                {
+                    firstBlock = newBlocks[i];
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
     }
