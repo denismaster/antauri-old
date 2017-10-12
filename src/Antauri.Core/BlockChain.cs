43ec157005a3bc6edf060686f8be8fa4abafd354
denismaster <denismaster@outlook.com>
2017-10-12 21:06:10 +0300

    Add some methods

--- src/Antauri.Core/BlockChain.cs
+++ src/Antauri.Core/BlockChain.cs
@@ -1,17 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Antauri.Core
 {
     public class BlockChain
     {
         private List<Block> _blocks;
+        private readonly IHashProvider _hasher;
 
-        public BlockChain()
+        public BlockChain(IHashProvider hasher)
         {
-            this._blocks = new List<Block>() { Block.GenesisBlock };
+            _hasher = hasher ?? throw new ArgumentNullException(nameof(hasher));
+
+            _blocks = new List<Block>() { Block.GenesisBlock };
         }
 
         public List<Block> Blocks => _blocks;
+
+        public Block LatestBlock => _blocks[_blocks.Count - 1];
+
+        public void addBlock(Block newBlock)
+        {
+            if (IsValidNewBlock(newBlock, LatestBlock))
+            {
+                _blocks.Add(newBlock);
+            }
+        }
+
+        public bool IsValidNewBlock(Block newBlock, Block previousBlock)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CalculateHash(int index, string previousHash, long timestamp, string data)
+        {
+            var builder = new StringBuilder(index);
+            builder.Append(previousHash).Append(timestamp).Append(data);
+            return _hasher.Hash(builder.ToString());
+        }
+
     }
 }
