7b23b936bb7597ce2146f0d3fd06c5f6e93e82cc
Denis Obolensky <denis_obolensky@xrm.ru>
2017-10-13 11:49:07 +0300

    Add basic controller implementation

---
+++ src/Antauri.Node/Controllers/BlocksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Antauri.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Antauri.Node.Controllers
+{
+    [Route("api/blocks")]
+    public class BlocksController : Controller
+    {
+        private BlockChain _blockChain;
+
+        public BlocksController(BlockChain blockChain){
+            this._blockChain = blockChain ?? throw new ArgumentNullException(nameof(blockChain));
+        }
+        // GET api/blocks
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_blockChain.Blocks);
+        }
+
+        // GET api/blocks/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/blocks/mine
+        [HttpPost("mine")]
+        public void Post([FromBody]string value)
+        {
+        }
+    }
+}
--- src/Antauri.Node/Startup.cs
+++ src/Antauri.Node/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Antauri.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +25,8 @@ namespace Antauri.Node
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSingleton<BlockChain>();
+            services.AddSingleton<IHashProvider, SHA256HashProvider>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
