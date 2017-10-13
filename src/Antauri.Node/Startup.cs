3ecf15650050381385c76c44ee519a886ca092b9
Denis Obolensky <denis_obolensky@xrm.ru>
2017-10-13 16:12:12 +0300

    Add basic WebSocket middleware. Must be redoned to use Blockchain

--- src/Antauri.Node/Startup.cs
+++ src/Antauri.Node/Startup.cs
@@ -32,6 +32,9 @@ namespace Antauri.Node
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseWebSockets();
+            app.UseMiddleware<WebSocketService>();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
