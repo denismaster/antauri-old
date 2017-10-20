5691ac5282f7ec1d7835f44714febf0bee488e29
denismaster <denismaster@outlook.com>
2017-10-20 23:41:21 +0300

    Enable test support

---
+++ tests/Antauri.Core.Tests/BlockChainTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Antauri.Core.Tests
+{
+    public class BlockChainTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
