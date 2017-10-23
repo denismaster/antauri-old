fe26f24c58be4ad56e83d13e38ae1e1dc4c0fce0
Denis Obolensky <denis_obolensky@xrm.ru>
2017-10-23 17:00:54 +0300

    Add AspNetCore integration project

---
+++ src/Antauri.AspNetCore/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Antauri.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Antauri.AspNetCore
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddAntauri(this IServiceCollection services)
+        {
+            services.AddSingleton<BlockChain>();
+            services.AddSingleton<IHashProvider, SHA256HashProvider>();
+            return services;
+        }
+    }
+}
