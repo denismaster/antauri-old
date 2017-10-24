6c3b3460961262b2db5b69e51ebcd4c187024fbd
denismaster <denismaster@outlook.com>
2017-10-24 15:00:56 +0300

    Add test for Genesis block

---
+++ tests/Antauri.Core.Tests/BlockTest.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace Antauri.Core.Tests
+{
+    public class BlockTest
+    {
+        [Fact]
+        public void GenesisBlockTest()
+        {
+            var block = Block.GenesisBlock;
+
+            var blockData = new BlockData<string>()
+            {
+                Data = block.Data,
+                PreviousHash = block.PreviousHash,
+                Index = block.Index,
+                TimeStamp = block.TimeStamp
+            };
+
+            IHashProvider hashProvider = new SHA256HashProvider();
+
+            string hash = hashProvider.Hash(blockData);
+
+            Assert.Equal(hash, block.Hash);
+        }
+    }
+}
