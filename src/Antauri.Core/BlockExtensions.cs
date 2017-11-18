082725653f4516df8eac94539a64daef498875c5
Denis Obolensky <denis_obolensky@xrm.ru>
2017-11-18 15:36:10 +0300

    feat(Core): Added extension method to deconstruct blocks

---
+++ src/Antauri.Core/BlockExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace Antauri.Core
+{
+    public static class BlockExtensions
+    {
+        public static void Deconstruct<THeader, TData>(this IBlockBase<THeader, TData> block,
+            out THeader header, out TData data)
+            where THeader: IBlockHeader
+        {
+            header = block.Header;
+            data = block.Data;
+        }
+    }
+}
